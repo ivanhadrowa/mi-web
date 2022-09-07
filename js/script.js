9a3d6da648badccaeee1274ba204443a98ac916f
ivanhadrowa <ivanhadrowa0@gmail.com>
2022-09-07 10:53:00 -0300

    agregar al carro

--- js/script.js
+++ js/script.js
@@ -14,3 +14,16 @@ function animatedBars(){
     desplegable.classList.toggle("activemenu__desplegable");
 }
 
+let products = [];
+let total = 0;
+
+function add(product, price){
+    console.log(product, price);
+    products.push(product);
+    total = total + price;
+    document.getElementById('checkout').innerHTML = `Pagar $${total}`
+}
+
+function pay(){
+    window.alert(products.join(", \n"));
+}
